3cd10dacd41d9f9d3cfbac8d70f3fcca390cf338
CrypticCortex <kalyanguru18@gmail.com>
2025-01-18 05:09:45 +0530

    fix

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,11 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements {
-      // Add any custom elements or extend existing ones here
-      // For example:
-      // 'custom-element': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
+    type IntrinsicElements = object
   }
 }
